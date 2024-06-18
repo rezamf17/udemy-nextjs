1ff48a4ede0bf404c2c7e461a6e9a4314cb9226c
Reza M Fauzi <rezayuyara@gmail.com>
2024-06-18 21:58:50 +0700

    image optimalization with next

--- components/main-header.js
+++ components/main-header.js
@@ -1,24 +1,27 @@
 import Link from "next/link"
+import Image from "next/image"
 import logoImg from "@/assets/logo.png"
 import classes from "./main-header.module.css"
 
-export default function MainHeader(){
-    return (
-        <header className={classes.header}>
-            <h1>NextLevel Food</h1>
-            <Link className={classes.logo} href="/">
-                <img src={logoImg.src} alt="logo" />
-            </Link>
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        <Link href="/meals">Browser Meals</Link>
-                    </li>
-                    <li>
-                        <Link href="/community">Foods Community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+export default function MainHeader() {
+	return (
+		<header className={classes.header}>
+			<Link className={classes.logo} href="/">
+				<Image src={logoImg.src} alt="logo" priority
+					width={200}
+					height={200} />
+				NextLevel Food
+			</Link>
+			<nav className={classes.nav}>
+				<ul>
+					<li>
+						<Link href="/meals">Browser Meals</Link>
+					</li>
+					<li>
+						<Link href="/community">Foods Community</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	)
 }
