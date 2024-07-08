373a72337056325af7c37101b5b5f47acd61c00f
reza.muhamad <reza.muhamad@pactindo.com>
2024-07-08 16:31:47 +0700

    useformstatus

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,6 @@
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
@@ -42,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your Image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
